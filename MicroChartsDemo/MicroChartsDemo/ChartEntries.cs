1d8e79ff926583aa511ee773b1215fac51fe1787
akartikus <sitraka.razafimamy@gmail.com>
2021-07-15 22:28:30 +0200

    Add first charts

---
+++ MicroChartsDemo/MicroChartsDemo/ChartEntries.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Entry = Microcharts.ChartEntry;
+
+namespace MicroChartsDemo
+{
+    public class ChartEntries
+    {
+        private List<Entry> entries { get; set; }
+        private int pointLimit;
+        private SkiaSharp.SKColor graphColor;
+
+        public ChartEntries(int pointLimit, string color)
+        {
+            graphColor = SkiaSharp.SKColor.Parse(color);
+            entries = new List<Entry>();
+            for(int i = 0; i< pointLimit; i++)
+            {
+                entries.Add(GenerateChartEntry(0));
+            }
+            // FIXME : Unused?
+            this.pointLimit = pointLimit;
+        }
+
+        public List<Entry> AddPoint(int value)
+        {
+            entries.RemoveAt(0);
+            entries.Add(GenerateChartEntry(value));
+            return entries;
+        }
+
+
+        public List<Entry> GetEntries()
+        {
+            return entries;
+        }
+
+        private Entry GenerateChartEntry(int value)
+        {
+            return new Entry(value)
+            {
+                Color = graphColor,
+                TextColor = graphColor,
+                Label = "",
+                ValueLabel = value + ""
+            };
+        }
+    }
+    
+}
---
+++ MicroChartsDemo/MicroChartsDemo/MainPage.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Entry = Microcharts.ChartEntry;
+using Microcharts;
+namespace MicroChartsDemo
+{
+
+    public partial class MainPage : ContentPage
+    {
+
+        private static readonly SkiaSharp.SKColor GraphColor = SkiaSharp.SKColor.Parse("#16b302");
+        private ChartEntries chartEntries1;
+        private ChartEntries chartEntries2;
+        public MainPage()
+        {
+            InitializeComponent();
+            chartEntries1 = new ChartEntries(50, "#06c406");
+            chartEntries2 = new ChartEntries(50, "#fcfc30");
+            Chart1.Chart = new LineChart
+            {
+                Entries = chartEntries1.GetEntries(),
+                LineMode = LineMode.Straight,
+                PointMode = PointMode.None,
+                LineSize = 8,
+                BackgroundColor = SkiaSharp.SKColors.Transparent
+            };
+
+            Chart2.Chart = new LineChart
+            {
+                Entries = chartEntries2.GetEntries(),
+                LineMode = LineMode.Straight,
+                PointMode = PointMode.None,
+                LineSize = 8,
+                BackgroundColor = SkiaSharp.SKColors.Transparent
+            };
+
+        }
+        void OnButtonClicked(object sender, EventArgs e)
+        {
+            Random random = new Random();
+
+            Chart1.Chart.Entries = chartEntries1.AddPoint(random.Next(-100, 100));
+            Chart2.Chart.Entries = chartEntries2.AddPoint(random.Next(-100, 100));
+
+
+        }
+
+    }
+}
